305a359883d369fb8222f19ad644759636b7aa03
Keno Dressel <hire@keno.digital>
2024-12-20 08:55:36 +0100

    fix(faq): removes old code

--- src/components/faq.tsx
+++ src/components/faq.tsx
@@ -2,55 +2,6 @@ import * as React from 'react';
 
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/ui/accordion';
 
-export interface FAQItemProps {
-    question: string;
-    iconSrc: string;
-}
-
-export interface FAQListProps {
-    items: FAQItemProps[];
-}
-export const FAQItem: React.FC<FAQItemProps> = ({ question }) => {
-    return (
-        <>
-            <div className='flex w-full flex-wrap items-center justify-between gap-10 max-md:max-w-full'>
-                <div className='my-auto min-w-[240px] self-stretch'>{question}</div>
-                <span className='my-auto aspect-square w-6 cursor-pointer text-4xl font-normal'>+</span>
-            </div>
-            <div className='mt-5 max-w-full self-end border-t'></div>
-        </>
-    );
-};
-export const FAQList: React.FC<FAQListProps> = ({ items }) => {
-    return (
-        <div className='flex min-w-[240px] flex-1 shrink basis-0 flex-col text-lg font-medium leading-none max-md:max-w-full'>
-            {items.map((item, index) => (
-                <div key={index} className='mt-5 first:mt-0'>
-                    <FAQItem question={item.question} iconSrc={item.iconSrc} />
-                </div>
-            ))}
-        </div>
-    );
-};
-
-const faqItems: FAQItemProps[] = [
-    {
-        question: 'What are the hardware requirements?',
-        iconSrc:
-            'https://cdn.builder.io/api/v1/image/assets/TEMP/3755ba18787f15b97813e1db7ee026bbb2edf07283769ef5ce9b13dcece63f36?placeholderIfAbsent=true&apiKey=93d96d41fa4e473b9b2c8dacbe6d22b6'
-    },
-    {
-        question: 'How does hyperchain token economy work?',
-        iconSrc:
-            'https://cdn.builder.io/api/v1/image/assets/TEMP/3755ba18787f15b97813e1db7ee026bbb2edf07283769ef5ce9b13dcece63f36?placeholderIfAbsent=true&apiKey=93d96d41fa4e473b9b2c8dacbe6d22b6'
-    },
-    {
-        question: 'Is there a risk of attack with dPoW?',
-        iconSrc:
-            'https://cdn.builder.io/api/v1/image/assets/TEMP/3755ba18787f15b97813e1db7ee026bbb2edf07283769ef5ce9b13dcece63f36?placeholderIfAbsent=true&apiKey=93d96d41fa4e473b9b2c8dacbe6d22b6'
-    }
-];
-
 export const FAQ: React.FC = () => {
     return (
         <div className='flex flex-col justify-center bg-stone-50 px-32 py-16 text-neutral-800 max-md:px-5 lg:px-52'>
