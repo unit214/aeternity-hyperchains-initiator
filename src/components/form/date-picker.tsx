ced9154b87157fe6996b02d678753f4ee631ea15
Timo Erdelt <timo@unit214.de>
2024-12-23 15:22:15 +0100

    refactor(initiator): generate hour options in hours picker

--- src/components/form/date-picker.tsx
+++ src/components/form/date-picker.tsx
@@ -54,56 +54,11 @@ export const DayPicker: React.FC<DatePickerProps> = ({ value, onChange }) => {
     );
 };
 
-const hoursOptions = [
-    '00:00',
-    '00:30',
-    '01:00',
-    '01:30',
-    '02:00',
-    '02:30',
-    '03:00',
-    '03:30',
-    '04:00',
-    '04:30',
-    '05:00',
-    '05:30',
-    '06:00',
-    '06:30',
-    '07:00',
-    '07:30',
-    '08:00',
-    '08:30',
-    '09:00',
-    '09:30',
-    '10:00',
-    '10:30',
-    '11:00',
-    '11:30',
-    '12:00',
-    '12:30',
-    '13:00',
-    '13:30',
-    '14:00',
-    '14:30',
-    '15:00',
-    '15:30',
-    '16:00',
-    '16:30',
-    '17:00',
-    '17:30',
-    '18:00',
-    '18:30',
-    '19:00',
-    '19:30',
-    '20:00',
-    '20:30',
-    '21:00',
-    '21:30',
-    '22:00',
-    '22:30',
-    '23:00',
-    '23:30'
-];
+const hourOptions = Array.from(
+    { length: 48 },
+    (_, i) => `${String(Math.floor(i / 2)).padStart(2, '0')}:${i % 2 === 0 ? '00' : '30'}`
+);
+
 export const HoursPicker: React.FC<DatePickerProps> = ({ value, onChange }) => {
     const handleOnSelect = (option: string) => {
         const hours = parseInt(option.split(':')[0]);
@@ -119,7 +74,7 @@ export const HoursPicker: React.FC<DatePickerProps> = ({ value, onChange }) => {
                     <SelectValue defaultValue={format(value, 'HH:mm')} placeholder='Start Time' />
                 </SelectTrigger>
                 <SelectContent>
-                    {hoursOptions.map((option, key) => (
+                    {hourOptions.map((option, key) => (
                         <SelectItem key={key} value={option}>
                             {option}
                         </SelectItem>
