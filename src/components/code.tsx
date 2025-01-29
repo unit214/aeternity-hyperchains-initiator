d094b8cf50832ddd1b81c6cabf8e5f2d444def28
Timo Erdelt <timo@unit214.de>
2025-01-29 15:27:50 +0100

    fix: add break-all to code component to fix layout on last page

--- src/components/code.tsx
+++ src/components/code.tsx
@@ -1,5 +1,9 @@
 import type { ReactNode } from 'react';
 
 export const Code = ({ children }: { children: ReactNode }) => {
-    return <div className='text-normal inline-flex w-fit rounded-lg bg-gray-200 px-2 py-1 font-mono'>{children}</div>;
+    return (
+        <div className='text-normal inline-flex w-fit break-all rounded-lg bg-gray-200 px-2 py-1 font-mono'>
+            {children}
+        </div>
+    );
 };
